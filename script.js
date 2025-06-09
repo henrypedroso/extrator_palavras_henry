3c5f4a9a4b02e7fdffd1729ac86d394a9ea50ad6
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2025-06-09 07:26:06 -0300

    ssssssssssssss

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 function mostraPalavrasChave() { 
   const texto = document.querySelector('#entrada-de-texto').value;
   const campoResultado = document.querySelector('#resultado-palavrachave');
+  const palavras = texto.split(" ");
 
-  campoResultado.textContent = texto;
+  campoResultado.textContent = palavras;
+
+
+
 } 
